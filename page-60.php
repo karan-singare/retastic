2109f07b7ae0fca7540c6f977d868be2b1715f4d
Karan Singh Singare <karansingare@gmail.com>
2021-07-14 17:57:23 +0530

    custom testimonials

---
+++ page-60.php
@@ -0,0 +1,63 @@
+<!--
+  * Custom Page for Tesimonials Construction
+-->
+<?php get_header(); ?>
+
+<?php get_template_part('partials/slider'); ?>
+
+<?php
+/**
+* Setup query to show the testimonial’
+*/
+  $args = array(
+    'post_type' => 'testimonial',
+    'post_status' => 'publish',
+    'orderby' => 'date',
+    'order' => 'ASC',
+  );
+
+  $loop = new WP_Query( $args );
+
+ ?>
+
+  <main class="service testimonials">
+
+    <div class="row">
+      <div class="col-2-of-3">
+
+          <?php while($loop->have_posts() ) : $loop->the_post(); ?>
+
+            <div class="story">
+              <figure class="story__shape">
+                <?php if (has_post_thumbnail()): ?>
+                  <img src="<?php echo the_post_thumbnail_url(); ?>" alt="Person on tour" class="story__img">
+                <?php else: ?>
+                  <img src="<?php echo get_template_directory_uri() . '/images/about-1.jpg'; ?>" alt="Person on tour" class="story__img">
+                <?php endif; ?>
+
+                <figcaption class="story__caption"><?php the_excerpt(); ?></figcaption>
+              </figure>
+              <div class="story__text">
+                <h3 class="heading__tertiary u-margin-bottom-small"><?php the_title(); ?></h3>
+                <?php the_content(); ?>
+              </div>
+            </div>
+
+          <?php endwhile; ?>
+
+      </div>
+      <div class="col-1-of-3">
+        <?php echo do_shortcode("[fluentform id='2']"); ?>
+      </div>
+    </div>
+
+    <?php wp_reset_postdata(); ?>
+
+
+
+  </main>
+
+
+
+
+<?php get_footer(); ?>
--- partials/stories.php
+++ partials/stories.php
@@ -1,3 +1,19 @@
+<?php
+/**
+* Setup query to show the testimonial’
+*/
+$args = array(
+  'post_type' => 'testimonial',
+  'post_status' => 'publish',
+  'orderby' => 'date',
+  'order' => 'ASC',
+  'posts_per_page' => 2,
+);
+
+$loop = new WP_Query( $args );
+
+ ?>
+
 <section class="section-stories">
       <div class="u-center-text">
         <h2 class="heading__secondary u-margin-bottom-big">
@@ -5,7 +21,30 @@
         </h2>
       </div>
 
-      <div class="row">
+      <?php while($loop->have_posts() ) : $loop->the_post(); ?>
+        <div class="row">
+          <div class="story">
+            <figure class="story__shape">
+              <?php if (has_post_thumbnail()): ?>
+                <img src="<?php echo the_post_thumbnail_url(); ?>" alt="Person on tour" class="story__img">
+              <?php else: ?>
+                <img src="<?php echo get_template_directory_uri() . '/images/about-1.jpg'; ?>" alt="Person on tour" class="story__img">
+              <?php endif; ?>
+
+              <figcaption class="story__caption"><?php the_excerpt(); ?></figcaption>
+            </figure>
+            <div class="story__text">
+              <h3 class="heading__tertiary u-margin-bottom-small"><?php the_title(); ?></h3>
+              <?php the_content(); ?>
+            </div>
+          </div>
+        </div>
+
+      <?php endwhile; ?>
+
+
+
+      <!-- <div class="row">
         <div class="story">
           <figure class="story__shape">
             <img src="<?php echo get_template_directory_uri() . '/images/about-1.jpg'; ?>" alt="Person on tour" class="story__img">
@@ -33,9 +72,11 @@
             </p>
           </div>
         </div>
-      </div>
+      </div> -->
 
       <div class="u-center-text">
-        <a href="#" class="btn-text">Read all stories &rarr;</a>
+        <a href="/testimonials" class="btn-text">Read all stories &rarr;</a>
       </div>
     </section>
+
+<?php wp_reset_postdata(); ?>
